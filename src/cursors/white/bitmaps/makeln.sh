7ebefb7babe2765518137c9180917bbd5804d3b5
TDCMC <69730881+TDC@users.noreply.github.com>
2026-05-04 19:37:49 +0330

    Added white version of the cursors

---
+++ src/cursors/white/bitmaps/makeln.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -s left_ptr ../../../Noir/cursors/arrow
+ln -s left_ptr ../../../Noir/cursors/default
+ln -s move ../../../Noir/cursors/fleur
+ln -s hand1 ../../../Noir/cursors/grab
+ln -s question_arrow ../../../Noir/cursors/help
+ln -s circle ../../../Noir/cursors/no-drop
+ln -s crossed_circle ../../../Noir/cursors/not-allowed
+ln -s hand2 ../../../Noir/cursors/pointer
+ln -s left_ptr_watch ../../../Noir/cursors/progress
+ln -s xterm ../../../Noir/cursors/text
+ln -s left_ptr ../../../Noir/cursors/top_left_arrow
+ln -s watch ../../../Noir/cursors/wait
